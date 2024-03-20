5a7ec89aa37f9243d3f75217d77aef6fb5d5bd20
tordvb10 <tordvb10.kodehode@gmail.com>
2024-03-20 14:13:37 +0100

    ferdig med noe feilsøking for useState

---
+++ src/hook/fetchAPI.js
@@ -0,0 +1,37 @@
+//endpoints must start with "/"
+import { useState, useEffect } from "react";
+
+export function fetchAPI(endpoint, url = `https://api.realworld.io/api`) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const isLoading = data === null && error === null;
+  const option = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+    },
+  };
+  console.log(`${url}${endpoint}`);
+  useEffect(() => {
+    async function fetching() {
+      try {
+        const response = await fetch(`${url}${endpoint}`, option);
+        if (!response.ok) {
+          setError("Server responded with:", response.status);
+          return;
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        setError("An error occured, please refresh the page");
+        console.error(error);
+      }
+    }
+    fetching();
+  }, []);
+  return {
+    isLoading,
+    data,
+    error,
+  };
+}
