c86045ae6a5248687015a2080195a74e12fadace
NordikE <erik.b10.kodehode@gmail.com>
2024-03-21 15:00:15 +0100

    Article.jsx skeleton incl basic CSS

---
+++ src/components/article.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./article.module.css";
+
+export const Article = () => {
+  return (
+    <div className={style.article}>
+      <h2>Article</h2>
+    </div>
+  );
+};
