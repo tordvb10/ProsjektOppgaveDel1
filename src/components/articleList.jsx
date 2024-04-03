0aff71fd8f891e7c0455a0bb84536ede305d1d5c
NordikE <erik.b10.kodehode@gmail.com>
2024-04-03 10:52:41 +0200

    Nav bar content

--- src/components/articleList.jsx
+++ src/components/articleList.jsx
@@ -9,26 +9,28 @@ export const ArticleList = () => {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div className={style.articleList}>
-      <ul>
-        {articles &&
-          articles.map((article) => (
-            <li key={article.slug}>
-              <h4>{article.author}</h4>
-              <p>{article.date}</p>
-              <h2>{article.name}</h2>
-              <h3>{article.slug}</h3>
-              <ul>
-                {article.tags &&
-                  article.tags.map((tag) => (
-                    <li key={tag}>
-                      <p>{tag}</p>
-                    </li>
-                  ))}
-              </ul>
-            </li>
-          ))}
-      </ul>
-    </div>
+    <>
+      <div className={style.articleList}>
+        <ul>
+          {articles &&
+            articles.map((article) => (
+              <li key={article.slug}>
+                <h4>{article.author}</h4>
+                <p>{article.date}</p>
+                <h2>{article.name}</h2>
+                <h3>{article.slug}</h3>
+                <ul>
+                  {article.tags &&
+                    article.tags.map((tag) => (
+                      <li key={tag}>
+                        <p>{tag}</p>
+                      </li>
+                    ))}
+                </ul>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </>
   );
 };
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,7 +3,16 @@ import style from "./nav.module.css";
 export const NavBar = () => {
   return (
     <div className={style.nav}>
-      <h2>Nav-bar</h2>
+      <div className="Navbar">
+        <h2>conduit</h2>
+        <button id="home">Home</button>
+        <button id="sign-in">Sign In</button>
+        <button id="sign-up">Sign Up</button>
+      </div>
+      <div className={style.nav - header}>
+        <h1>conduit</h1>
+        <h3>A place to share your knowledge.</h3>
+      </div>
     </div>
   );
 };
