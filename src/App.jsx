b34c1bab443087473c14fd1aa14bc76bce3c4d37
tordvb10 <tordvb10.kodehode@gmail.com>
2024-04-04 09:34:43 +0200

    fikset routing, var bare en variabelfeil som gjorde problemet. Har også lagt til mapper hvor man kan lage json filer ved fetches.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 //import { fetchAPI } from "./hook/fetchAPI";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-import { LandingPage } from "./Pages/LandingPage";
-import { LayoutRoot } from "./Pages/LayoutRoot";
-import { PageNotFound } from "./Pages/PageNotFound";
+import { LandingPage } from "./Pages/LandingPage.jsx";
+import { LayoutRoot } from "./Pages/LayoutRoot.jsx";
+import { PageNotFound } from "./Pages/PageNotFound.jsx";
 
 function App() {
   /* comment the section under out to see that fetchAPI only works in dev as explained in its document. */
--- src/Pages/LayoutRoot.jsx
+++ src/Pages/LayoutRoot.jsx
@@ -1,4 +1,4 @@
-import { Outlet,NavLink } from "react-router-dom"
+import { Outlet, NavLink } from "react-router-dom"
 export function LayoutRoot(){
     return (
         <>
@@ -6,10 +6,10 @@ export function LayoutRoot(){
                 <nav>
                     <ul>
                         <li>
-                            <Navlink
+                            <NavLink
                             to="/"
                             >
-                            </Navlink>
+                            </NavLink>
                         </li>
                     </ul>
                 </nav>
