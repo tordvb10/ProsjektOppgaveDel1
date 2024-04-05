d89ba43b19fb785412c2a0042924f21e3b4aae8f
NordikE <erik.b10.kodehode@gmail.com>
2024-04-05 10:56:04 +0200

    Search component started

---
+++ src/components/search.jsx
@@ -0,0 +1,8 @@
+export const Searchbar = () => {
+  return (
+    <div>
+      <input type="text" id="search-field" name="search-field" />
+      <button id="search-button">Search</button>
+    </div>
+  );
+};
