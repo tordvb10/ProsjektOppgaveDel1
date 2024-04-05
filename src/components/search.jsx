617bcaa8d21bbd77d09c51169626a1ab9f27c9cb
NordikE <erik.b10.kodehode@gmail.com>
2024-04-05 12:07:24 +0200

    Search Searching somewhat done

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,8 +1,25 @@
-export const Searchbar = () => {
+import React, { useState } from "react";
+
+export const Searchbar = ({ onSearch }) => {
+  const [input, setInput] = useState("");
+
+  const handleSearch = () => {
+    onSearch(input);
+  };
+
   return (
     <div>
-      <input type="text" id="search-field" name="search-field" />
-      <button id="search-button">Search</button>
+      <input
+        type="text"
+        id="search-field"
+        name="search-field"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyPress={(e) => e.key === "Enter" && handleSearch()}
+      />
+      <button id="search-button" onClick={handleSearch}>
+        Search
+      </button>
     </div>
   );
 };
