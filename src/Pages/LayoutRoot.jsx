6fc436360ad4579aede502e67bf7e8c972baa561
NordikE <erik.b10.kodehode@gmail.com>
2024-04-06 19:05:26 +0200

    Font link

--- src/Pages/LayoutRoot.jsx
+++ src/Pages/LayoutRoot.jsx
@@ -1,27 +1,22 @@
-import { Outlet, NavLink } from "react-router-dom"
-export function LayoutRoot(){
-    return (
-        <>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet"></link>
-            <header>
-                <nav>
-                    <h2>conduit</h2>
-                    <ul>
-                        <li>
-                            <NavLink
-                            to="/"
-                            > HOME
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <Outlet/>
-            <footer>
-                <p>ProsjektOppgaveDel2</p>
-            </footer>
-        </>
-    )
+import { Outlet, NavLink } from "react-router-dom";
+
+export function LayoutRoot() {
+  return (
+    <>
+      <header>
+        <nav>
+          <h2>conduit</h2>
+          <ul>
+            <li>
+              <NavLink to="/">HOME</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+      <footer>
+        <p>ProsjektOppgaveDel2</p>
+      </footer>
+    </>
+  );
 }
