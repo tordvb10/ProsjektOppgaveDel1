e30ed4f638cbd1c7f38a37bf3c9ce8351312cbc3
NordikE <erik.b10.kodehode@gmail.com>
2024-04-06 19:01:24 +0200

    Styling på riktig plass. Glemte at du ikke ville ha Nav som komponent

--- src/Pages/LayoutRoot.jsx
+++ src/Pages/LayoutRoot.jsx
@@ -7,6 +7,7 @@ export function LayoutRoot(){
 <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet"></link>
             <header>
                 <nav>
+                    <h2>conduit</h2>
                     <ul>
                         <li>
                             <NavLink
