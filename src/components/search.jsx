794adc08754837701aa0d17c87183405b7014187
NordikE <erik.b10.kodehode@gmail.com>
2024-04-07 14:16:46 +0200

    Hadde vært greit å bruke samme navn...

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -4,25 +4,36 @@ import { searchArticles } from "../utilities/search/searchUtil";
 const SearchField = () => {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
+
   const newSearch = (event) => {
     const newQuery = event.target.value;
     setQuery(newQuery);
-    const filteredResults = searchItems(newQuery);
+
+    const filteredResults = searchArticles(newQuery);
     setResults(filteredResults);
   };
 
   return (
     <div>
-      <input type="text" value={query} onChange={newSearch} />
+      <input
+        type="text"
+        value={query}
+        onChange={newSearch}
+        placeholder="Search by slug, title, author, or tags"
+      />
       <div>
-        {results.map((item, index) => (
-          <div key={index}>
-            <h2>{item.title}</h2>
-            <p>Author: {item.author}</p>
-            <p>Slug: {item.slug}</p>
-            <p>Tags: {item.tagList.join(", ")}</p>
-          </div>
-        ))}
+        {results.length > 0 ? (
+          results.map((item, index) => (
+            <div key={index}>
+              <h2>{item.title}</h2>
+              <p>Author: {item.author}</p>
+              <p>Slug: {item.slug}</p>
+              <p>Tags: {item.tagList.join(", ")}</p>
+            </div>
+          ))
+        ) : (
+          <p>No results found</p>
+        )}
       </div>
     </div>
   );
