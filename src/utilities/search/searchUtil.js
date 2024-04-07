e37c29a79e0c8567ff41ae43a6422fdd7d6b8190
NordikE <erik.b10.kodehode@gmail.com>
2024-04-07 14:04:09 +0200

    Search utility

---
+++ src/utilities/search/searchUtil.js
@@ -0,0 +1,20 @@
+import items from "../jsonfiles/single-step-function.json";
+
+export function searchItems(query) {
+  const lowercasedQuery = query.toLowerCase();
+  return items.filter((item) => {
+    const matchesSlug = item.slug.toLowerCase().includes(lowercasedQuery);
+
+    const matchesTitle = item.title?.toLowerCase().includes(lowercasedQuery);
+    const matchesAuthor = item.author?.toLowerCase().includes(lowercasedQuery);
+    const matchesTagList = item.tagList?.some((tag) =>
+      tag.toLowerCase().includes(lowercasedQuery)
+    );
+    console.log(matchesSlug);
+    console.log(matchesTitle);
+    console.log(matchesAuthor);
+    console.log(matchesTagList);
+
+    return matchesSlug || matchesTitle || matchesAuthor || matchesTagList;
+  });
+}
