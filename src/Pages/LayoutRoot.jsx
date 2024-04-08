4bdf895354e36adde09a77f3072e33ba975fea7b
tordvb10 <tordvb10.kodehode@gmail.com>
2024-04-08 11:45:38 +0200

    hei

--- src/Pages/LayoutRoot.jsx
+++ src/Pages/LayoutRoot.jsx
@@ -1,39 +1,37 @@
-import { Outlet, NavLink } from "react-router-dom"
-export function LayoutRoot(){
-    const navinfo = [
-        {
-            TO: "/",
-            DisplayName: "HOME"
-        },
-        {
-            TO: "/article/",
-            DisplayName: "ARTICLE"
-        }
-    ]
-    return (
-        <>
-            <header>
-                <nav>
-                    <ul>
-                        {
-                            navinfo.map((info)=>{
-                                return(
-                                    <li>
-                                        <NavLink
-                                        to={info.TO}
-                                        > {info.DisplayName}
-                                        </NavLink>
-                                    </li>
-                                )                                
-                            })
-                        }                        
-                    </ul>
-                </nav>
-            </header>
-            <Outlet/>
-            <footer>
-                <p>ProsjektOppgaveDel2</p>
-            </footer>
-        </>
-    )
+import { Outlet, NavLink } from "react-router-dom";
+import styles from "./LayoutRoot.module.css";
+
+export function LayoutRoot() {
+  const navinfo = [
+    {
+      TO: "/",
+      DisplayName: "HOME",
+    },
+    {
+      TO: "/article/",
+      DisplayName: "ARTICLE",
+    },
+  ];
+  return (
+    <>
+      <header>
+        <nav className={styles.nav}>
+          <h2>conduit</h2>
+          <ul>
+            {navinfo.map((info) => {
+              return (
+                <li key={info.DisplayName}>
+                  <NavLink to={info.TO}> {info.DisplayName}</NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+      <footer>
+        <p>ProsjektOppgaveDel2</p>
+      </footer>
+    </>
+  );
 }
