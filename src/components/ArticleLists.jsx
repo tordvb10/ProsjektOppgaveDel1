c4a7751834fe9a41434437199b30db5207d77afd
NordikE <erik.b10.kodehode@gmail.com>
2024-04-10 16:24:30 +0200

    Styling/ className

--- src/components/ArticleLists.jsx
+++ src/components/ArticleLists.jsx
@@ -1,20 +1,18 @@
-import style from "./articleList.module.css";
-import {articles} from "../utilities/jsonfiles/GET-articles-ALLKEYS.json"
+import style from "./ArticleLists.module.css";
+import { articles } from "../utilities/jsonfiles/GET-articles-ALLKEYS.json";
 import { ArticleBox } from "./ArticleBox.jsx";
 export function ArticleLists() {
   return (
     <>
-        <ul className={style.master_ul}>
-            {
-                articles.map((article) => {
-                    return (                        
-                        <li key={article.slug}>                            
-                            <ArticleBox ArticleElement={article} />
-                        </li>
-                    )
-                })
-            }
-        </ul>
+      <ul className={style.master_ul}>
+        {articles.map((article) => {
+          return (
+            <li key={article.slug}>
+              <ArticleBox ArticleElement={article} />
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
-};
+}
