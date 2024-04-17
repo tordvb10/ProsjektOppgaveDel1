dbee458227ce6ff9db24383af4a65b1f6919eda6
tordvb10 <tordvb10.kodehode@gmail.com>
2024-04-17 12:59:37 +0200

    skal gjøre tags på artikkel-listene responsive.

--- src/components/javascript/filterArticleBySlugs.js
+++ src/components/javascript/filterArticleBySlugs.js
@@ -1,4 +1,4 @@
-import { articles } from "../jsonfiles/GET-articles-ALLKEYS.json";
+import { articles } from "../../utilities/jsonfiles/GET-articles-ALLKEYS.json";
 export function filterArticleBySlugs(currentSlugs){
     return articles.filter((article)=>{
         return currentSlugs.includes(article.slug)
