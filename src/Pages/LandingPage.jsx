abea59e0ff36276c2d24e81cb4796896b5e6f6b3
NordikE <erik.b10.kodehode@gmail.com>
2024-04-18 12:07:28 +0200

    værsågod,tord

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,8 +2,8 @@ import styles from "./LandingPage.module.css";
 
 export function LandingPage() {
   return (
-    <>
+    <main>
       <p className={styles.para}>This is LandingPage</p>
-    </>
+    </main>
   );
 }
